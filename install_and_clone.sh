5d2cb8386a9d13e6e4579476da9986c8ef1b7552
Deepak42074 <51868020+Deepak42074@users.noreply.github.com>
2020-10-29 22:40:34 +0530

    Add files via upload

---
+++ install_and_clone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install -y ngspice
+sudo apt install git
+
+cd NgspiceNetlist/Prelayout/
+
+git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky13_fd_pr
+
+
+
+
+
+
+
+
